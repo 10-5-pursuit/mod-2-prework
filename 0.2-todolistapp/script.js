c093e434de95cdfe0e8d05e455447b05e896102d
David Cruz Esk <david.cruz.esk@gmail.com>
2024-02-19 22:15:00 -0500

    COMPLETE TO-D0 LIST ASSIGNMENT

---
+++ 0.2-todolistapp/script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function saveDate() {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must write something!')
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveDate();
+}
+
+listContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveDate();
+
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveDate();
+    }
+}, false);
+
+showTask();
