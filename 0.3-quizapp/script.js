5392c235fcb078ffc42db0af5db5e10ddbc1372e
sadya104 <sandrickson@pursuit.com>
2024-02-20 12:01:13 -0500

    How to make a quiz

---
+++ 0.3-quizapp/script.js
@@ -0,0 +1,66 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is smallest county in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: true},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    }
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next"
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+    
+}
+startQuiz();
