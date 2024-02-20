9664df3d3bed4937df2a40858aacfe733bd2f5c6
sadya104 <sandrickson@pursuit.com>
2024-02-20 01:35:32 -0500

    update todo list

--- 0.2-todolistapp/script.js
+++ 0.2-todolistapp/script.js
@@ -9,5 +9,29 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
     }
+    inputBox.value = "";
+    saveData();
 }
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
